f3b12d4c4e3b0120c42f429bf56b51c67661c575
RocioSosa21 <nicolle982010@gmail.com>
2025-06-06 23:01:13 -0300

    MTV informacion de la unidad 10

---
+++ MiProyecto/comandos.py
@@ -0,0 +1,13 @@
+# python -m venv .venv: crea el entorno virtual
+# .\.venv\Scripts\activate: activa la maquina virtual o entorno virtual
+# ctrl+shift+p -> python interpreter -> venv : para seleccionar el interprete de python de la maquina virtual
+# pip install django: instala django para usar
+# django-admin startproject MiProyecto . : para que django sea el entorno visual web
+# python manage.py startapp AppCoder: crea un proyecto o una nueva carpeta con toda la informacion neceseria para funcionar
+# en el archivo models.py modificamos la informacion para que puede crear una base de datos de estudiante y demas
+# python manage.py makemigrations y migrate: Para crear las tablas en la base de datos, ejecutamos las migraciones
+# creamos vistas para manejar las operaciones CRUD (Crear, Leer, Actualizar, Eliminar) para nuestros modelos, modificando el archivo .views de appcoder
+# python manage.py runserver para ejecutar el django y ver la pagina web
+# ctrl+c para apagar el servidor
+# git add . : para subir todo en estado 
+
